c8cc7265465fbab48043f2a0da1a1a46962abccc
Chinthan Rai <chinthanrai.123@gmail.com>
2026-05-04 12:47:20 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ import { sendEmail } from "./utils/sendEmail.js";
 dotenv.config();
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
@@ -82,5 +82,5 @@ setInterval(async () => {
   }
 }, 60 * 1000); // Check every minute
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
