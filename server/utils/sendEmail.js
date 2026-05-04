62d34539b2219ad5619518f2f919e62f645d403a
ChinthanRai <chinthanrai.123@gmail.com>
2026-05-04 21:35:11 +0530

    fix tls config

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -15,14 +15,17 @@ export const sendEmail = async ({ to, subject, html }) => {
 
   try {
     const transporter = nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT || 587,
-      secure: false, // true for 465, false for other ports
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-      }
-    });
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  secure: false, // important for port 587
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
+  }
+});
 
     const info = await transporter.sendMail({
       from: process.env.FROM_EMAIL || process.env.EMAIL_USER,
@@ -44,3 +47,4 @@ export const sendEmail = async ({ to, subject, html }) => {
     return { success: false, message: "Failed to send email" };
   }
 };
+
