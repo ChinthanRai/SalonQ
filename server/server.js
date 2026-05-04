a30ce52f916bf09ff37e1f301ed5fc7bc04234b9
ChinthanRai <chinthanrai.123@gmail.com>
2026-05-04 21:17:54 +0530

    debug email env logs

--- server/server.js
+++ server/server.js
@@ -84,3 +84,7 @@ setInterval(async () => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+console.log("EMAIL_USER:", process.env.EMAIL_USER);
+console.log("EMAIL_PASS:", process.env.EMAIL_PASS ? "SET" : "NOT SET");
+console.log("EMAIL_HOST:", process.env.EMAIL_HOST);
+console.log("EMAIL_PORT:", process.env.EMAIL_PORT);
