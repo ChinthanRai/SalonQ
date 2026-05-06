6ace46758dfeb88e28d647f1e77176b53b95dd09
ChinthanRai <chinthanrai.123@gmail.com>
2026-05-06 23:00:28 +0530

    Support dynamic secure flag for different SMTP ports

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -8,7 +8,7 @@ export const sendEmail = async ({ to, subject, html }) => {
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: Number(process.env.EMAIL_PORT), // also fix this
-      secure: false,
+      secure: Number(process.env.EMAIL_PORT) === 465,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
